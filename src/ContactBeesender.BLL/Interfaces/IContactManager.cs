3f3e77c05bde791fb57e7f5db530857cec976706
Hamidalion <fearofwar@mail.ru>
2020-12-16 21:50:56 +0300

    feat: add IContact manager

---
+++ src/ContactBeesender.BLL/Interfaces/IContactManager.cs
@@ -0,0 +1,55 @@
+﻿using ContactBeesender.BLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactBeesender.BLL.Interfaces
+{
+    /// <summary>
+    /// Manager of Contacts.
+    /// </summary>
+    public interface IContactManager
+    {
+        /// <summary>
+        /// Create contact by user identifier.
+        /// </summary>
+        /// <param name="contactDto">Contact data transfer object.</param>
+        Task CreateAsync(ContactDto contactDto);
+
+        /// <summary>
+        /// Get contact by identifier.
+        /// </summary>
+        /// <param name="id">Identifier.</param>
+        /// <param name="userId">User identifier.</param>
+        /// <returns>Contact data transfer objects.</returns>
+        Task<ContactDto> GetContactAsync(int id, string userId);
+
+        /// <summary>
+        /// Get contact by user identifier.
+        /// </summary>
+        /// <param name="userId">User identifier.</param>
+        /// <returns>List of Contact data transfer objects.</returns>
+        Task<IEnumerable<ContactDto>> GetContactAsync(string userId);
+
+        /// <summary>
+        /// Delete contact by identifier.
+        /// </summary>
+        /// <param name="id">Identifier.</param>
+        /// <param name="userId">User identifier.</param>
+        Task DeleteAsync(int id, string userId);
+
+        /// <summary>
+        /// Change contact status.
+        /// </summary>
+        /// <param name="id">Identifier.</param>
+        /// <param name="userId">User identifier.</param>
+        Task ChangeContactStatusAsync(int id, string userId);
+
+        /// <summary>
+        /// Update contact by identifier.
+        /// </summary>
+        /// <param name="contactDto">Contact data transfer object.</param>
+        Task UpdateAsync(ContactDto contactDto);
+    }
+}
---
+++ src/ContactBeesender.Web/Controllers/ContactController.cs
@@ -0,0 +1,179 @@
+﻿using ContactBeesender.BLL.Interfaces;
+using ContactBeesender.Web.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactBeesender.Web.Controllers
+{
+    [Authorize]
+    public class ContactController : Controller
+    {
+        private readonly IAccountManager _accountManager;
+        private readonly ITodoManager _todoManager;
+
+        public TodoController(
+            IAccountManager accountManager,
+            ITodoManager todoManager)
+        {
+            _accountManager = accountManager ?? throw new ArgumentNullException(nameof(accountManager));
+            _todoManager = todoManager ?? throw new ArgumentNullException(nameof(todoManager));
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var userId = await _accountManager.GetUserIdByNameAsync(User.Identity.Name);
+            var todoDtos = await _todoManager.GetTodosAsync(userId);
+
+            var todoViewModels = new List<ContactViewModel>();
+            foreach (var todoDto in todoDtos)
+            {
+                todoViewModels.Add(new TodoViewModel
+                {
+                    Id = todoDto.Id,
+                    Title = todoDto.Title,
+                    Description = todoDto.Description,
+                    Priority = todoDto.PriorityType.ValidatePriorityType(),
+                    IsActive = todoDto.IsActive,
+                    Created = todoDto.Created,
+                    Closed = todoDto.Closed
+                });
+            }
+
+            return View(todoViewModels);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            GeneratePriorityTypeList();
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(TodoActionViewModel model)
+        {
+            model = model ?? throw new ArgumentNullException(nameof(model));
+
+            if (ModelState.IsValid)
+            {
+                var userId = await _accountManager.GetUserIdByNameAsync(User.Identity.Name);
+                var todoDto = new TodoDto
+                {
+                    UserId = userId,
+                    Title = model.Title,
+                    Description = model.Description,
+                    PriorityType = PriorityTypeExtension.ToPriorityType(model.Priority)
+                };
+
+                await _todoManager.CreateAsync(todoDto);
+
+                return RedirectToAction("Index", "Todo");
+            }
+
+            GeneratePriorityTypeList();
+            return View(model);
+        }
+
+        public async Task<IActionResult> Detail(int id)
+        {
+            var userId = await _accountManager.GetUserIdByNameAsync(User.Identity.Name);
+            var todoDto = await _todoManager.GetTodoAsync(id, userId);
+
+            var todoViewModel = new TodoViewModel
+            {
+                Id = todoDto.Id,
+                Title = todoDto.Title,
+                Description = todoDto.Description,
+                Priority = todoDto.PriorityType.ToLocal(),
+                IsActive = todoDto.IsActive,
+                Created = todoDto.Created,
+                Closed = todoDto.Closed
+            };
+
+            return View(todoViewModel);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditAsync(int id)
+        {
+            var userId = await _accountManager.GetUserIdByNameAsync(User.Identity.Name);
+            var todoDto = await _todoManager.GetTodoAsync(id, userId);
+
+            var todoActionViewModel = new TodoActionViewModel
+            {
+                Id = todoDto.Id,
+                Title = todoDto.Title,
+                Description = todoDto.Description,
+                Priority = (int)todoDto.PriorityType,
+            };
+
+            GeneratePriorityTypeList();
+            return View(todoActionViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(TodoActionViewModel model)
+        {
+            model = model ?? throw new ArgumentNullException(nameof(model));
+
+            if (ModelState.IsValid)
+            {
+                var userId = await _accountManager.GetUserIdByNameAsync(User.Identity.Name);
+                var todoDto = new TodoDto
+                {
+                    Id = model.Id,
+                    UserId = userId,
+                    Title = model.Title,
+                    Description = model.Description,
+                    PriorityType = PriorityTypeExtension.ToPriorityType(model.Priority)
+                };
+
+                await _todoManager.UpdateAsync(todoDto);
+
+                return RedirectToAction("Index", "Todo");
+            }
+
+            GeneratePriorityTypeList();
+            return View(model);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var userId = await _accountManager.GetUserIdByNameAsync(User.Identity.Name);
+            await _todoManager.DeleteAsync(id, userId);
+
+            return RedirectToAction("Index", "Todo");
+        }
+
+        public async Task<IActionResult> Complete(int id)
+        {
+            var userId = await _accountManager.GetUserIdByNameAsync(User.Identity.Name);
+            await _todoManager.ChangeTodoStatusAsync(id, userId);
+
+            return RedirectToAction("Index", "Todo");
+        }
+
+        [NonAction]
+        private void GeneratePriorityTypeList()
+        {
+            IEnumerable<PriorityTypeModel> priorityTypes = new List<PriorityTypeModel>
+            {
+                new PriorityTypeModel { Id = (int)PriorityType.Low, Type = PriorityType.Low.ToLocal() },
+                new PriorityTypeModel { Id = (int)PriorityType.Medium, Type = PriorityType.Medium.ToLocal() },
+                new PriorityTypeModel { Id = (int)PriorityType.High, Type = PriorityType.High.ToLocal() }
+            };
+
+            ViewBag.PriorityTypes = new SelectList(priorityTypes, "Id", "Type");
+        }
+
+
+
+
+    }
+}
